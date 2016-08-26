9b8a3c6470063761154c2f13015e90946c695186
asd <jekadeshi4@gmail.com>
2016-08-26 16:58:39 +0300

    asda

--- Test/src/ua/Main.java
+++ Test/src/ua/Main.java
@@ -3,10 +3,13 @@ package ua;
 public class Main {
 
 	public static void main(String[] args) {
+		print();
+
+	}
+	public static void print(){
 		System.out.println("Hello");
 		System.out.println("World");
 		System.out.println("bla-bla");
-
 	}
 
 }
